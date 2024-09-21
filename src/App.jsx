f042abaa3ac9355414a6f70ac33558ff9dcc6d27
afayyaz1 <afayyaz1@swarthmore.edu>
2024-09-21 15:58:32 -0400

    Refactor App component and add BoxWithForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Box from './components/Box'
+import BoxWithForm from './components/BoxWithForm'
 
 function App() {
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center">
-      <div className="">
-        <Box text="d. Waitlist" />
+      <div className="pb-6">
+        <Box/>
+      </div>
+      <div className="p-4">
+        <h1 className="font-bold text-8xl">Coming Soon!</h1>
+      </div>
+      <div className="pt-6">
+        <BoxWithForm/>
       </div>
     </div>
   )
